7d4e442a010ee6910d7b4beb3958bb01eafa1c18
kallith <karlt13@ru.is>
2015-12-02 17:12:44 +0000

    small mod for init server

---
+++ flutningur/init_server.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+python3 manage.py makemigrations population
+python3 manage.py migrate
+# Get d3 and nvd3 libs
+if [ -d lib/nvd3 ] ; then
+    cd lib
+    wget https://github.com/novus/nvd3/zipball/master -O nvd3.zip
+    mkdir nvd3
+    unzip nvd3 -d nvd3
+    subdir=$(ls nvd3)
+    mv nvd3/$subdir/* nvd3
+    rmdir nvd3/$subdir
+    rm nvd3.zip
+fi
+if [ -d lib/d3 ] ; then
+    wget https://github.com/mbostock/d3/releases/download/v3.5.10/d3.zip -O d3.zip
+    mkdir d3
+    unzip d3.zip -d d3
+    rm d3.zip
+fi
--- flutningur/population/views.py
+++ flutningur/population/views.py
@@ -1,4 +1,14 @@
+import os.path
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
+    print(request)
+    data = open(os.path.dirname(os.path.abspath(__file__)) + "/templates/population/data").read()
+    template = loader.get_template("population/area_line_plot.html")
+    context = RequestContext(request, { 'data' : data }, processors = [])
+    return HttpResponse(template.render(context))
+
+
+def index2(request):
     return HttpResponse("Hello, world. You're at the population db")
