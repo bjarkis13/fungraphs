74e56c856771c854c147c3130eaa2db12ff9cc43
kallith <karlt13@ru.is>
2015-12-08 23:52:38 +0000

    Cartogram moved to django

--- flutningur/carto/views.py
+++ flutningur/carto/views.py
@@ -6,5 +6,16 @@ from population.models import Municipality, Population
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Cartogram index")
+    #raw = Population.objects.filter(year=int(id_year)).filter(municipality__mid__isnull=False).order_by('municipality__name', 'year')
+    # if len raw .,
+    data = {}
+    #for line in raw:
+    #    name = line.municipality.name
+    #    if not name in data:
+    #        data[name] = []
+    #    data[name].append((line.municipality.mid,line.val))
+    template = loader.get_template("carto/cartogram.html")
+    #return HttpResponse("".join(["{}({}):{}, ".format(m.municipality.name, m.year, m.val) for m in data]))
+    context = RequestContext(request, { 'data' : data }, processors = [])
+    return HttpResponse(template.render())
 
