61d668486f21db53bd0ce9698d6d14e9c952ebe4
kallith <karlt13@ru.is>
2015-12-08 22:27:09 +0000

    heatmap moved to map

--- flutningur/heat/urls.py
+++ flutningur/heat/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^(?P<id_year>[0-9]{4})/$', views.plot),
+    #url(r'^(?P<args>[0-9]{4}(,[0-9]{4})*)/$', views.plot),
     url(r'^preset/(?P<group>[0-9]+)/$', views.preset),
-    url(r'^(?P<args>[0-9]{4}(,[0-9]{4})*)/$', views.plot),
 ]
 
--- flutningur/heat/views.py
+++ flutningur/heat/views.py
@@ -8,8 +8,20 @@ from population.models import Municipality, Population
 def index(request):
     return HttpResponse("heat map index")
 
-def plot(request, args):
-    return HttpResponse("heat map " + args)
-
 def preset(request, group):
     return plot(request, "group" + group)
+
+def plot(request, id_year="1900"):
+    raw = Population.objects.filter(year=int(id_year)).filter(municipality__mid__isnull=False).order_by('municipality__name', 'year')
+    # if len raw .,
+    data = {}
+    for line in raw:
+        name = line.municipality.name
+        if not name in data:
+            data[name] = []
+        data[name].append((line.municipality.mid,line.val))
+    template = loader.get_template("heat/heatmap.html")
+    #return HttpResponse("".join(["{}({}):{}, ".format(m.municipality.name, m.year, m.val) for m in data]))
+    context = RequestContext(request, { 'data' : raw }, processors = [])
+    return HttpResponse(template.render(context))
+
