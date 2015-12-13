c829c84a7236b3636484526c8d5c2c2e14ceb0fe
kallith <karlt13@ru.is>
2015-12-13 15:50:29 +0000

    Colors and legend added to choropleth, and region colors added to map

--- flutningur/heat/views.py
+++ flutningur/heat/views.py
@@ -25,15 +25,23 @@ def preset(request, group):
     return plot(request, "group" + group)
 
 def plot(request):
+    groups = [300, 1000, 5000, 30000]
+    def calc_group(n):
+        for i,g in zip(range(len(groups)),groups):
+            if n < g:
+                return i
+        return len(groups)
+
     raw = Population.objects.filter(municipality__mid__isnull=False).order_by('year')
     data = {}
     for obj in raw:
         if obj.year not in data:
             data[obj.year] = []
-        data[obj.year].append(( obj.municipality.name, obj.municipality.mid, obj.val ))
+        data[obj.year].append(( obj.municipality.name, obj.municipality.mid, obj.val, calc_group(obj.val)))
     lis = []
     for key in data:
         lis.append((key, data[key]))
+        lis[-1][1].sort(key=lambda x:x[1])
     lis.sort(key=lambda x:x[0])
     # if len raw .,
     template = loader.get_template("heat/heatmap.html")
@@ -42,6 +50,7 @@ def plot(request):
         'title': "arst",
         'subtitle' : '{} choropleth'.format('Population'),
         'choroplethdata' : lis,
+        'groups': groups,
         'subbanner' : True,
         'subbanner_data' : [("By population", "/maps/", True)],
         'css' : ["mystyle.css"],
