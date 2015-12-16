752534808571c6cb84d0e0ff81691623b26aba53
kallith <karlt13@ru.is>
2015-12-16 01:03:11 +0000

    plot index text makes more sense

--- flutningur/plot/views.py
+++ flutningur/plot/views.py
@@ -14,17 +14,24 @@ def index(request):
 
 def scale(request, scale='linear'):
     args = ",".join([str(m.mid) for m in Municipality.objects.filter(mid__isnull=False)])
-    return plot(request, args, scale=scale)
+    return plot(request, args, scale=scale, what="each of the current municipalities", name='')
 
 def preset(request, group, scale='linear'):
     if int(group) == 0:
         lis = [str(r) for r in get_good_mid()]
+        what = "municipality with persistent growth"
+        name = "persistent growth"
     elif int(group) == 9:
         lis = [str(r) for r in get_bad_mid()]
+        what = "municipality with persistent depopulation"
+        name = "persistent depopulation"
     else:
         lis = [str(m.mid) for m in Municipality.objects.filter(region__id=int(group))]
+        what = "municipality within the region {}".format(Regions.objects.get(id=int(group)).name)
+        name = Regions.objects.get(id=int(group)).name
     args = ",".join(lis)
-    return plot(request, args, scale=scale)
+    print(name)
+    return plot(request, args, scale=scale, what=what, name=name)
 
 def aggregate_regions():
     raw = Population.objects.filter(municipality__mid__isnull=False).values_list('municipality__region__id','year').annotate(Sum('val'))
@@ -34,24 +41,24 @@ def aggregate_regions():
     return res
 
 def plot_regions(request,scale='linear'):
-    return response(request, aggregate_regions(), scale)
+    return response(request, aggregate_regions(), scale, what="region of iceland", name="regions")
 
-def plot(request, args, scale='linear'):
+def plot(request, args, scale='linear', what=None, name=None):
     mun_lis = [int(s) for s in args.split(',')]
     raw = Population.objects.filter(municipality__mid__in=mun_lis).order_by('municipality__name', 'year')
     data = {}
     for line in raw:
-        name = line.municipality.name
-        if not name in data:
-            data[name] = []
-        data[name].append((line.year,line.val))
+        cname = line.municipality.name
+        if not cname in data:
+            data[cname] = []
+        data[cname].append((line.year,line.val))
     lis = []
     for key in data:
         lis.append((key, data[key]))
     lis.sort(key=lambda x: IS_sort()(x[0]))
-    return response(request, lis, scale)
+    return response(request, lis, scale, what, name)
 
-def response(request, lis, scale):
+def response(request, lis, scale, what, name):
     islog = scale == 'log'
     lineplot_d = {
             "values": lis,
@@ -60,6 +67,7 @@ def response(request, lis, scale):
             "hidelegend":len(lis) > 20,
             "linewidth":"4px",
             "opacity": 0.14 if len(lis) > 20 else 0.6,
+            "what": what,
             "y" : {
                 "name":"Population",
                 "format": ",",
@@ -69,7 +77,8 @@ def response(request, lis, scale):
 
     template = loader.get_template("plot/index.html")
     context = RequestContext(request, {
-                'title' : 'Population {}line plot'.format("logarithmic " if islog else ""),
+                'title' : 'Population {}line plot, {}'
+                    .format("logarithmic " if islog else "", name if name is not None else "custom"),
                 'plotactive': True,
                 'lineplot' : lineplot_d,
                 'css' : [ "lib/nvd3/build/nv.d3.min.css" ],
